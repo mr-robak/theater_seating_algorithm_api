5afdc83f319a471a31137a317664c60162b60762
Marcin Robak <mr.robak@gmail.com>
2021-01-18 21:20:03 +0100

    display theater layout

---
+++ index.js
@@ -0,0 +1,34 @@
+const { theater, groupOfUsers } = require("./data");
+
+console.log("groupOfUsers", groupOfUsers);
+
+// console.log(JSON.stringify(theater[1], null, 2));
+
+function theaterLayout(theater) {
+  for (section = 1; section <= Object.keys(theater).length; ++section) {
+    //displays section name
+    console.log(" ----------- ");
+    console.log(`| ${theater[section].name} |`);
+    console.log(" ----------- ");
+
+    let currentRowRank = 0;
+    for (
+      rowNr = 1;
+      rowNr <= Object.keys(theater[section].rows).length;
+      ++rowNr
+    ) {
+      //displays row rank
+      if (theater[section].rows[rowNr].rank != currentRowRank) {
+        currentRowRank = theater[section].rows[rowNr].rank;
+        console.log("Rank:", currentRowRank);
+      }
+
+      //displays row with seat numbers
+      console.log(
+        theater[section].rows[rowNr].seats.map((seat) => seat.number).toString()
+      );
+    }
+  }
+}
+
+theaterLayout(theater);
