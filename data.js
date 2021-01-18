67154d6851106cfcc45a5786da7967a0926c1c76
Marcin Robak <mr.robak@gmail.com>
2021-01-18 19:32:16 +0100

    create a data model

---
+++ data.js
@@ -0,0 +1,221 @@
+const groupOfUsers = [1, 3, 4, 4, 5, 1, 2, 4];
+
+const theater = {
+  1: {
+    name: "Main Hall",
+    rows: {
+      1: {
+        rank: 1,
+        seats: [
+          { number: 1, status: null, feature: ["front row", "aisle"] },
+          { number: 2, status: null, feature: ["front row"] },
+          { number: 3, status: null, feature: ["front row"] },
+          { number: 4, status: null, feature: ["front row"] },
+          { number: 5, status: null, feature: ["front row"] },
+          { number: 6, status: null, feature: ["front row"] },
+          { number: 7, status: null, feature: ["front row"] },
+          { number: 8, status: null, feature: ["front row", "aisle"] },
+        ],
+      },
+
+      2: {
+        rank: 1,
+        seats: [
+          { number: 1, status: null, feature: ["aisle"] },
+          { number: 2, status: null, feature: [] },
+          { number: 3, status: null, feature: [] },
+          { number: 4, status: null, feature: [] },
+          { number: 5, status: null, feature: [] },
+          { number: 6, status: null, feature: [] },
+          { number: 7, status: null, feature: [] },
+          { number: 8, status: null, feature: ["aisle"] },
+        ],
+      },
+      3: {
+        rank: 1,
+        seats: [
+          { number: 1, status: null, feature: ["aisle"] },
+          { number: 2, status: null, feature: [] },
+          { number: 3, status: null, feature: [] },
+          { number: 4, status: null, feature: [] },
+          { number: 5, status: null, feature: [] },
+          { number: 6, status: null, feature: [] },
+          { number: 7, status: null, feature: [] },
+          { number: 8, status: null, feature: ["aisle"] },
+        ],
+      },
+      4: {
+        rank: 2,
+        seats: [
+          { number: 1, status: null, feature: ["aisle"] },
+          { number: 2, status: null, feature: [] },
+          { number: 3, status: null, feature: [] },
+          { number: 4, status: null, feature: [] },
+          { number: 5, status: null, feature: [] },
+          { number: 6, status: null, feature: [] },
+          { number: 7, status: null, feature: [] },
+          { number: 8, status: null, feature: ["aisle"] },
+        ],
+      },
+
+      5: {
+        rank: 2,
+        seats: [
+          { number: 1, status: null, feature: ["aisle"] },
+          { number: 2, status: null, feature: [] },
+          { number: 3, status: null, feature: [] },
+          { number: 4, status: null, feature: [] },
+          { number: 5, status: null, feature: [] },
+          { number: 6, status: null, feature: [] },
+          { number: 7, status: null, feature: [] },
+          { number: 8, status: null, feature: ["aisle"] },
+        ],
+      },
+      6: {
+        rank: 3,
+        seats: [
+          { number: 1, status: null, feature: ["aisle"] },
+          { number: 2, status: null, feature: [] },
+          { number: 3, status: null, feature: [] },
+          { number: 4, status: null, feature: [] },
+          { number: 5, status: null, feature: [] },
+          { number: 6, status: null, feature: [] },
+          { number: 7, status: null, feature: [] },
+          { number: 8, status: null, feature: ["aisle"] },
+        ],
+      },
+      7: {
+        rank: 3,
+        seats: [
+          { number: 1, status: null, feature: ["aisle"] },
+          { number: 2, status: null, feature: [] },
+          { number: 3, status: null, feature: [] },
+          { number: 4, status: null, feature: [] },
+          { number: 5, status: null, feature: [] },
+          { number: 6, status: null, feature: [] },
+          { number: 7, status: null, feature: [] },
+          { number: 8, status: null, feature: ["aisle"] },
+        ],
+      },
+    },
+  },
+  2: {
+    name: "1st Balcony",
+    rows: {
+      1: {
+        rank: 1,
+        seats: [
+          { number: 1, status: null, feature: ["front row", "aisle"] },
+          { number: 2, status: null, feature: ["front row"] },
+          { number: 3, status: null, feature: ["front row"] },
+          { number: 4, status: null, feature: ["front row"] },
+          { number: 5, status: null, feature: ["front row"] },
+          { number: 6, status: null, feature: ["front row"] },
+          { number: 7, status: null, feature: ["front row"] },
+          { number: 8, status: null, feature: ["front row", "aisle"] },
+        ],
+      },
+
+      2: {
+        rank: 2,
+        seats: [
+          { number: 1, status: null, feature: ["aisle"] },
+          { number: 2, status: null, feature: [] },
+          { number: 3, status: null, feature: [] },
+          { number: 4, status: null, feature: [] },
+          { number: 5, status: null, feature: [] },
+          { number: 6, status: null, feature: [] },
+          { number: 7, status: null, feature: [] },
+          { number: 8, status: null, feature: ["aisle"] },
+        ],
+      },
+      3: {
+        rank: 2,
+        seats: [
+          { number: 1, status: null, feature: ["aisle"] },
+          { number: 2, status: null, feature: [] },
+          { number: 3, status: null, feature: [] },
+          { number: 4, status: null, feature: [] },
+          { number: 5, status: null, feature: [] },
+          { number: 6, status: null, feature: [] },
+          { number: 7, status: null, feature: [] },
+          { number: 8, status: null, feature: ["aisle"] },
+        ],
+      },
+      4: {
+        rank: 3,
+        seats: [
+          { number: 1, status: null, feature: ["aisle"] },
+          { number: 2, status: null, feature: [] },
+          { number: 3, status: null, feature: [] },
+          { number: 4, status: null, feature: [] },
+          { number: 5, status: null, feature: [] },
+          { number: 6, status: null, feature: [] },
+          { number: 7, status: null, feature: [] },
+          { number: 8, status: null, feature: ["aisle"] },
+        ],
+      },
+    },
+  },
+  3: {
+    name: "2nd Balcony",
+    rows: {
+      1: {
+        rank: 1,
+        seats: [
+          { number: 1, status: null, feature: ["front row", "aisle"] },
+          { number: 2, status: null, feature: ["front row"] },
+          { number: 3, status: null, feature: ["front row"] },
+          { number: 4, status: null, feature: ["front row"] },
+          { number: 5, status: null, feature: ["front row"] },
+          { number: 6, status: null, feature: ["front row"] },
+          { number: 7, status: null, feature: ["front row"] },
+          { number: 8, status: null, feature: ["front row", "aisle"] },
+        ],
+      },
+
+      2: {
+        rank: 2,
+        seats: [
+          { number: 1, status: null, feature: ["aisle"] },
+          { number: 2, status: null, feature: [] },
+          { number: 3, status: null, feature: [] },
+          { number: 4, status: null, feature: [] },
+          { number: 5, status: null, feature: [] },
+          { number: 6, status: null, feature: [] },
+          { number: 7, status: null, feature: [] },
+          { number: 8, status: null, feature: ["aisle"] },
+        ],
+      },
+      3: {
+        rank: 2,
+        seats: [
+          { number: 1, status: null, feature: ["aisle"] },
+          { number: 2, status: null, feature: [] },
+          { number: 3, status: null, feature: [] },
+          { number: 4, status: null, feature: [] },
+          { number: 5, status: null, feature: [] },
+          { number: 6, status: null, feature: [] },
+          { number: 7, status: null, feature: [] },
+          { number: 8, status: null, feature: ["aisle"] },
+        ],
+      },
+      4: {
+        rank: 3,
+        seats: [
+          { number: 1, status: null, feature: ["aisle"] },
+          { number: 2, status: null, feature: [] },
+          { number: 3, status: null, feature: [] },
+          { number: 4, status: null, feature: [] },
+          { number: 5, status: null, feature: [] },
+          { number: 6, status: null, feature: [] },
+          { number: 7, status: null, feature: [] },
+          { number: 8, status: null, feature: ["aisle"] },
+        ],
+      },
+    },
+  },
+};
+
+module.exports.theater = theater;
+module.exports.groupOfUsers = groupOfUsers;
