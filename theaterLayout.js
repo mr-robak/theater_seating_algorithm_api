2ccdb87070388f91e675e11d1057cb85e1665501
Marcin Robak <mr.robak@gmail.com>
2021-01-19 20:30:22 +0100

    fill up sits sequentialy, no preferences

--- theaterLayout.js
+++ theaterLayout.js
@@ -16,16 +16,90 @@ module.exports = {
         //displays row rank
         if (theater[section].rows[rowNr].rank != currentRowRank) {
           currentRowRank = theater[section].rows[rowNr].rank;
-          console.log("Rank:", currentRowRank);
+          console.log(" Rank:", currentRowRank);
         }
 
         //displays row with seat numbers
         console.log(
+          " ",
           theater[section].rows[rowNr].seats
-            .map((seat) => seat.number)
-            .toString()
+            .map((seat) => (seat.status != null ? `[${seat.status}]` : "[ ]"))
+            .join("")
         );
       }
     }
+  },
+  findIndexOfNextEmptySeat: function (row, rowNr) {
+    function getIndex(seats) {
+      return seats.findIndex((seat) => seat.status === null);
+    }
+    //for even row numbers search from right to left
+    if (rowNr % 2 === 0) {
+      console.log("even row");
+      const reversedRow = [...row.seats].reverse();
+
+      index =
+        getIndex(reversedRow) === -1
+          ? -1
+          : row.seats.length - 1 - getIndex(reversedRow);
+      console.log("found index", index);
+    } else {
+      console.log("odd row");
+      //for odd row numbers search from left to right
+      index = getIndex(row.seats);
+      console.log("found index", index);
+    }
+
+    return index;
+  },
+
+  seatingNoPreferences: function (theater, group, rank) {
+    // theater - layout data model
+    // group - number of people in a group
+    // rank - desired seat rank
+    let seatsCounter = group;
+    const numberOfSections = Object.keys(theater).length;
+
+    //loop through sections
+    for (section = 1; section <= numberOfSections; ++section) {
+      const numberOfRows = Object.keys(theater[section].rows).length;
+      //loop through rows
+      let rowNr = 1;
+      while (rowNr <= numberOfRows) {
+        // console.log("Row:", rowNr);
+        const row = theater[section].rows[rowNr];
+        //check if current row matches the rank
+        if (row.rank === rank) {
+          //find seats for the group members
+          let index = this.findIndexOfNextEmptySeat(row, rowNr);
+          // console.log("seatsCounter", seatsCounter);
+
+          // if no free space is found jump to the next row in this loop
+          if (index === -1) {
+            console.log(
+              `no seats in row ${row}, section: ${theater[section].name}`
+            );
+            console.log("continue");
+            ++rowNr;
+          } else {
+            // change status of found seat from null to a group number
+            theater[section].rows[rowNr].seats[index].status = group;
+            console.log(
+              11111,
+              `found free seat - section: ${theater[section].name} row: ${rowNr} / index: ${index}`
+            );
+            --seatsCounter;
+            console.log("seatsCounter", seatsCounter);
+          }
+        }
+        if (seatsCounter === 0) {
+          console.log("returning theater");
+          return theater;
+        }
+        // ++rowNr;
+      }
+    }
+
+    return theater;
   },
 };
