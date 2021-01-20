5bab0129c98575b2c3e44449ee0b7bd2b3f0fa8b
Marcin Robak <mr.robak@gmail.com>
2021-01-20 15:19:44 +0100

    move tests to a separate file

--- index.js
+++ index.js
@@ -18,20 +18,3 @@ app.listen(port, () => {
   
   Listening at localhost:${port}`);
 });
-
-const theaterLayout = require("./theaterLayout"); //contains methods used to operate on theater layout (printing, finding seats, etc)
-
-// const { theater } = require("./data");
-
-const groupOfUsers = [1, 3, 4, 1, 5, 1, 2, 4, 5, 2, 4, 3, 2];
-// const groupOfUsers = [1, 3, 4, 4, 5, 1, 2, 4 ];
-// const groupOfUsers = [1, 3, 4];
-
-// const newLayout = theaterLayout.seatingNoPreferences(theater, 28, 1);
-
-const newLayout = groupOfUsers.reduce((acc, curr) => {
-  return theaterLayout.seatingNoPreferences(acc, curr, 1);
-}, theater);
-
-// console.log(newLayout);
-theaterLayout.print(newLayout, false);
