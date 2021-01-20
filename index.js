37090728819bf042753cd12cafc10ea9bc2d2ea9
Marcin Robak <mr.robak@gmail.com>
2021-01-20 14:57:56 +0100

    set up express server , cors and json parser

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-const { theater } = require("./data");
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+const bodyParser = require("body-parser");
 
-// console.log("theater", theater);
-// console.log("groupOfUsers", groupOfUsers);
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const theater = JSON.parse(fs.readFileSync("./db/theater_data.json"));
+const event = JSON.parse(fs.readFileSync("./db/event_data.json"));
+
+app.listen(port, () => {
+  console.log(`
+  
+  Listening at localhost:${port}`);
+});
 
 const theaterLayout = require("./theaterLayout"); //contains methods used to operate on theater layout (printing, finding seats, etc)
+
+// const { theater } = require("./data");
 
 const groupOfUsers = [1, 3, 4, 1, 5, 1, 2, 4, 5, 2, 4, 3, 2];
 // const groupOfUsers = [1, 3, 4, 4, 5, 1, 2, 4 ];
@@ -16,4 +34,4 @@ const newLayout = groupOfUsers.reduce((acc, curr) => {
 }, theater);
 
 // console.log(newLayout);
-theaterLayout.print(newLayout);
+theaterLayout.print(newLayout, false);
